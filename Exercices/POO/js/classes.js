d01995c1897caed8886edf5d76639321c2382211
Mickael <mickaelmarchand@protonmail.com>
2024-11-13 16:05:20 +0100

    poo js

--- Exercices/POO/js/classes.js
+++ Exercices/POO/js/classes.js
@@ -1,13 +1,15 @@
 export class Jsonplaceholder {
-  constructor(link) {
+  constructor(link, limit, place) {
     this.link = link;
+    this.limit = "?_limit=" + limit;
+    this.place = place;
   }
 
-  show() {
-    fetch(this.link)
+  ShowText() {
+    fetch(this.link + this.limit)
       .then((response) => response.json())
       .then((data) => {
-        const sectionTest = document.getElementById("testt_section");
+        const sectionTest = document.getElementById(this.place);
         sectionTest.innerHTML = "";
 
         data.forEach((post) => {
@@ -21,11 +23,11 @@ export class Jsonplaceholder {
       });
   }
 
-  showdeux() {
-    fetch(this.link)
+  ShowImg() {
+    fetch(this.link + this.limit)
       .then((response) => response.json())
       .then((data) => {
-        const sectionTest = document.getElementById("test_section");
+        const sectionTest = document.getElementById(this.place);
         sectionTest.innerHTML = "";
 
         data.forEach((post) => {
@@ -44,5 +46,75 @@ export class Jsonplaceholder {
       .catch((error) => {
         console.error("Erreur:", error);
       });
+  }
+}
+
+export let array = {
+  test: "test",
+  test1: "autre",
+  number1: 10,
+  number2: 486,
+};
+
+// ---------
+
+export class OtherClass {
+  #private_number = 50;
+  public_number = 10;
+  yy;
+
+  constructor(number) {
+    this.number = number;
+    this.k;
+  }
+
+  calcul(private_number, public_number, number, yy) {
+    this.yy = this.#private_number + 10 + this.public_number + this.number;
+    return this.yy;
+  }
+
+  nocalcul() {
+    return this.yy + 2;
+  }
+
+  ifif() {
+    if (this.yy >= 50) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
+// ----------
+
+export class TestText {
+  constructor(texttitre, para, where) {
+    this.texttitre = texttitre;
+    this.where = where;
+    this.para = para;
+  }
+
+  _textInsert() {
+    return (document.getElementById(this.where).innerHTML =
+      "<h1>" + this.texttitre + "</h1> <p>" + this.para + "</p>");
+  }
+
+  textinin() {
+    return this._textInsert();
+  }
+  number() {
+    return 45;
+  }
+}
+
+export class TesTestText extends TestText {
+  constructor(texttitre, para, where, other) {
+    super(texttitre, para, where);
+    this.other = other;
+    this.number();
+  }
+
+  otherfun(other) {
+    return 4 + this.other + this.number();
   }
 }
--- Exercices/POO/js/script.js
+++ Exercices/POO/js/script.js
@@ -1,19 +1,56 @@
 import { Jsonplaceholder } from "./classes.js";
+import { array } from "./classes.js";
+import { OtherClass } from "./classes.js";
+import { TestText } from "./classes.js";
+import { TesTestText } from "./classes.js";
 
 const jsonPlaceholder = new Jsonplaceholder(
-  "https://jsonplaceholder.typicode.com/posts?_limit=10"
+  "https://jsonplaceholder.typicode.com/posts",
+  10,
+  "test_section_text"
 );
-jsonPlaceholder.show();
+jsonPlaceholder.ShowText();
 
 const jsonPlaceholderr = new Jsonplaceholder(
-  "https://jsonplaceholder.typicode.com/photos?_limit=200"
+  "https://jsonplaceholder.typicode.com/photos",
+  10,
+  "test_section_img"
 );
-jsonPlaceholderr.showdeux();
+jsonPlaceholderr.ShowImg();
 
-const imgElements = document.getElementsByClassName("img");
+// ----------------
 
-for (let imgElement of imgElements) {
-  imgElement.addEventListener("mouseover", () => {
-    console.log("test");
-  });
-}
+console.log(array);
+
+console.log(array["number1"]);
+
+console.log(array.number1);
+
+// -------------------
+
+const OtherC = new OtherClass(12);
+console.log(OtherC.calcul());
+console.log(OtherC.nocalcul());
+console.log(OtherC.ifif());
+
+// -------------
+
+const newnew = new TestText(
+  "Test titre",
+  "Test paragraphe",
+  "test_section_retext"
+);
+
+newnew._textInsert();
+
+console.log(newnew.textinin());
+
+const other = new TesTestText(
+  "Test titre deux",
+  "Test paragraphe deux",
+  "test_section_retext",
+  8
+);
+other._textInsert("");
+
+console.log(other.otherfun());
